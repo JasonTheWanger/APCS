5d68b7c4c5c287013b735085a7136257c4252e96
JasonTheWanger <59749543+JasonTheWanger@users.noreply.github.com>
2022-09-13 12:31:42 -0700

    Add files via upload

---
+++ Lesson1_1.java
@@ -0,0 +1,9 @@
+
+public class Lesson1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("\tHello world, my name is Jason. \nI want to take this course because my future major preference is Electrical Computer Engineer.");
+	}
+
+}
